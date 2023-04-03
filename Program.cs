96637bc96b121ac3448950fbe258a0b16b30fea5
nnamenatchx <122606565+nnamenatchx@users.noreply.github.com>
2023-04-03 10:39:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program {
+    static void Main(){
+         double[] numbers = new double[1000];
+        int count = 0; 
+        string mode;
+
+        string input;
+        do {
+            Console.Write("Enter a number or 'End' to stop: ");
+            input = Console.ReadLine();
+
+            if (input != "End") {
+                numbers[count] = double.Parse(input);
+                count++;
+            }
+        } while (input != "End");
+    }
+}
